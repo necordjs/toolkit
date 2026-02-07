2c12a4dd628e1f218beefcd0f8ce480e34dea587
SocketSomeone <socket.someone@gmail.com>
2026-02-07 05:39:28 +0300

    fix: remove deprecated code

--- src/instrument.ts
+++ src/instrument.ts
@@ -7,7 +7,6 @@ import {
 import { Logger } from '@nestjs/common';
 import { resourceFromAttributes } from '@opentelemetry/resources';
 import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } from '@opentelemetry/semantic-conventions';
-import { metrics } from '@opentelemetry/api';
 import * as Sentry from '@sentry/nestjs';
 import { nodeProfilingIntegration } from '@sentry/profiling-node';
 import { SentryPropagator, SentrySampler, SentrySpanProcessor } from '@sentry/opentelemetry';
